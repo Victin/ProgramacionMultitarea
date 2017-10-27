9aa342d0280cb6b1a4ac18460c882f13e485e8b5
Viti <victorgomezes@hotmail.com>
2017-10-27 12:04:09 +0200

    Numero Aleatorio v2

---
+++ Multiproceso/src/procesos/Aleatorios.java
@@ -0,0 +1,17 @@
+package procesos;
+
+import java.util.Scanner;
+
+
+public class Aleatorios {
+
+	public static void main(String[] args) {
+		
+		int min = Integer.parseInt(args[0]);
+		int max = Integer.parseInt(args[1]);
+		
+		int valorEntero = (int) Math.floor(Math.random()*(max- min + 1) + min); //M menor que N, ambos incluidos
+		
+		System.out.println(valorEntero);
+	}
+}
--- Multiproceso/src/procesos/Ejercicio3.java
+++ Multiproceso/src/procesos/Ejercicio3.java
@@ -1,10 +1,64 @@
 package procesos;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String opcion = null;
+		do {
+			System.out.println("1-)Para ejecutar el proceso teclee 'get'");
+			System.out.println("2-)Para salir teclee 'end'");
+			opcion = sc.next();
+			
+			switch(opcion) {
+				case "get": 
+					System.out.println("Numero minimo");
+					String minimo = sc.next();
+					System.out.println("Numero maximo");
+					String maximo = sc.next();
+					
+					ProcessBuilder pb = new ProcessBuilder("java", "-jar", "C:\\Users\\alumno\\Documents\\Clase\\Eclipse\\GITProgramacionMultitarea\\Aleatorio.jar", minimo, maximo);
+					
+					try {
+						Process proceso = pb.start();
+				
+						InputStream is = proceso.getInputStream();
+						InputStreamReader isr = new InputStreamReader(is);
+						BufferedReader br =  new BufferedReader(isr);
+						String linea = null;
+						
+						while((linea = br.readLine())!= null) {
+							System.out.println(linea);
+						}
+						
+						is.close();
+					
+						int exitValue = proceso.waitFor();//Espera a que termine el proceso
+						System.out.println("Resultado de ejecucion ha sido: " + exitValue);
+						System.out.println("-------------------------------");
+					} catch (IOException e) {
+						System.out.println("Error entrada/salida");
+						System.exit(-2);
+					} catch (InterruptedException e) {
+						System.out.println("Proceso interrumpido");
+						System.exit(-3);
+					}
+					break;
+				case "end":
+					System.out.println("Goodbye");
+					break;
+			}
+		}while(!opcion.equals("end"));
+		
+		
 	}
-
 }
