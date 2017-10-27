895723a182d6459f73db114cef670bf97d799f95
Viti <victorgomezes@hotmail.com>
2017-10-27 09:40:44 +0200

    Llamar a un JAR v2

---
+++ Multiproceso/src/procesos/EjercicioProcessBuilderInputRedirect.java
@@ -0,0 +1,47 @@
+package procesos;
+
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import javax.swing.JOptionPane;
+
+
+public class EjercicioProcessBuilderInputRedirect {
+
+	public static void main(String[] args) {
+	
+		String texto = JOptionPane.showInputDialog("Introduce un texto");
+		
+		ProcessBuilder pb = new ProcessBuilder("java", "-jar", "C:\\Users\\alumno\\Documents\\Clase\\Eclipse\\GITProgramacionMultitarea\\Mayusculas2.jar", texto);
+		//ProcessBuilder acepta dos argumentos(args) en su declaracion
+		
+		try {
+			Process proceso = pb.start();
+
+			InputStream is = proceso.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader br =  new BufferedReader(isr);
+			String linea = null;
+			
+			System.out.println("La salida del proceso hijo es: ");
+			while((linea = br.readLine())!= null) {
+				System.out.println(linea);
+			}
+			
+			br.close();
+		
+			int exitValue = proceso.waitFor();//Espera a que termine el proceso
+			System.out.println("Resultado de ejecucion ha sido: " + exitValue);
+		} catch (IOException e) {
+			System.out.println("Error entrada/salida");
+			System.exit(-2);
+		} catch (InterruptedException e) {
+			System.out.println("Proceso interrumpido");
+			System.exit(-3);
+		}
+	}
+}
---
+++ Multiproceso/src/procesos/Mayusculas.java
@@ -0,0 +1,10 @@
+package procesos;
+
+public class Mayusculas {
+
+	public static void main(String[] args) {
+		
+		for(String e: args)
+			System.out.println(e.toUpperCase());
+	}
+}
